ace9f3b3d213a2f7058c996cb51df3fa03502bbc
kavyasri <djkavyasri.1318@gmail.com>
2025-10-29 10:37:36 +0530

    commiting of leaverequest entity after adding variables

--- src/main/java/com/example/Project/Leap/Entity/LeaveRequest.java
+++ src/main/java/com/example/Project/Leap/Entity/LeaveRequest.java
@@ -1,5 +1,17 @@
 package com.example.Project.Leap.Entity;
 
+import jakarta.persistence.Entity;
+import java.time.LocalDate;
+import lombok.Data;
+
+@Data
+@Entity
 public class LeaveRequest {
-    
+    private int id;
+    private int user_id;
+    private String leaveType;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String reason;
+    private String status="Pending"; 
 }
