03ac6f5c4540ae6e3c0c97a816484e978e281b63
kavyasri <djkavyasri.1318@gmail.com>
2025-10-29 09:39:30 +0530

    initial commit

---
+++ src/main/java/com/example/Project/Leap/ProjectLeapApplication.java
@@ -0,0 +1,13 @@
+package com.example.Project.Leap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLeapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectLeapApplication.class, args);
+	}
+
+}
