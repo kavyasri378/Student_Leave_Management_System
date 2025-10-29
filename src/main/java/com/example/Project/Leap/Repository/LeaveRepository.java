334a15d20ffecc4c7f767a6230c5e63b2054cf4a
kavyasri <djkavyasri.1318@gmail.com>
2025-10-29 11:08:25 +0530

    commit of leave repo

--- src/main/java/com/example/Project/Leap/Repository/LeaveRepository.java
+++ src/main/java/com/example/Project/Leap/Repository/LeaveRepository.java
@@ -1,5 +1,11 @@
 package com.example.Project.Leap.Repository;
 
-public class LeaveRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.Project.Leap.Entity.LeaveRequest;
+
+@Repository
+public interface LeaveRepository  extends JpaRepository<LeaveRequest, Integer>{
     
 }
