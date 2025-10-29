db91417b9a8278d2c74c317d7719bf6d375d99ba
kavyasri <djkavyasri.1318@gmail.com>
2025-10-29 11:38:52 +0530

    commit of user repo after delacration of user defined func

--- src/main/java/com/example/Project/Leap/Repository/UserRepository.java
+++ src/main/java/com/example/Project/Leap/Repository/UserRepository.java
@@ -7,5 +7,5 @@ import com.example.Project.Leap.Entity.User;
 
 @Repository
 public interface  UserRepository extends JpaRepository<User, Integer>{
-    
+    User findByEmailAndPassword(String email,String password);
 }
