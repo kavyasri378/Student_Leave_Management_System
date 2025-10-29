ce58c075c91f174ca781b5ee8d7650ec504a3e30
kavyasri <djkavyasri.1318@gmail.com>
2025-10-29 11:36:15 +0530

    commit of user service file after adding regier and login fun

--- src/main/java/com/example/Project/Leap/Service/UserService.java
+++ src/main/java/com/example/Project/Leap/Service/UserService.java
@@ -1,5 +1,24 @@
 package com.example.Project.Leap.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.Project.Leap.Entity.User;
+import com.example.Project.Leap.Repository.UserRepository;
+
+@Service
 public class UserService {
     
+    @Autowired
+    private UserRepository userRepo;
+
+    //register the new user(save the details in db)
+    public User register(User user){
+        return userRepo.save(user);
+    }
+
+    //login function
+    public User login(String email,String password){
+        User user= userRepo.
+    }
 }
