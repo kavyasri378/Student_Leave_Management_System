8457c5b11a0642b35b4c127f225eb19d1b6c2bdf
kavyasri <djkavyasri.1318@gmail.com>
2025-10-29 11:05:57 +0530

    commit of user repo

--- src/main/java/com/example/Project/Leap/Repository/UserRepository.java
+++ src/main/java/com/example/Project/Leap/Repository/UserRepository.java
@@ -1,5 +1,11 @@
 package com.example.Project.Leap.Repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.Project.Leap.Entity.User;
+
+@Repository
+public interface  UserRepository extends JpaRepository<User, Integer>{
     
 }
