bcad173f213b0f6fa341364d2781073eff22550e
kavyasri <djkavyasri.1318@gmail.com>
2025-10-29 11:01:57 +0530

    commit of Notification entity

--- src/main/java/com/example/Project/Leap/Entity/Notification.java
+++ src/main/java/com/example/Project/Leap/Entity/Notification.java
@@ -1,5 +1,23 @@
 package com.example.Project.Leap.Entity;
 
+import java.time.LocalDateTime;
+
+import org.springframework.aot.generate.Generated;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class Notification {
-    
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int id;
+    private int user_id;
+    private String message;
+    private LocalDateTime sentTime;
 }
