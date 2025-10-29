174ffe41f7b4657e87c84dbd178011cdfcf8c27f
kavyasri <djkavyasri.1318@gmail.com>
2025-10-29 12:54:29 +0530

    commit notification repo after adding user delacared func

--- src/main/java/com/example/Project/Leap/Repository/NotificationRepository.java
+++ src/main/java/com/example/Project/Leap/Repository/NotificationRepository.java
@@ -1,10 +1,12 @@
 package com.example.Project.Leap.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.Project.Leap.Entity.Notification;
 @Repository
 public interface  NotificationRepository extends JpaRepository<Notification,Integer>{
-    
+    List<Notification> findByUser_id(int user_id);
 }
